9d9ec7d74aac873e0d491be45f9d662cbf4e0cb3
VChvostkovaaa <chv0053@vsb.cz>
2025-05-14 11:02:24 +0200

    Update random_place.py

--- mujbalik/random_place.py
+++ mujbalik/random_place.py
@@ -1,34 +1,42 @@
 import numpy as np
+from typing import Tuple, List
 
-def generate_random_scenario(number_of_bodies, position_range, velocity_range, mass_range):
+
+def generate_random_scenario(number_of_bodies: int,
+                             position_range: Tuple[float, float],
+                             velocity_range: Tuple[float, float],
+                             mass_range: Tuple[float, float]) \
+        -> Tuple[List[str], np.ndarray, np.ndarray, np.ndarray]:
     """
     Funkce generuje náhodné scénáře pro simulaci, která bude náhodně generovat počáteční podmínky planet (polohy, rychlosti a hmotnosti)
     pro daný počet planet.
 
-
     Args:
-        number_of_bodies: kolik planet chci vygenerovat
-        position_range: dvojice čísel (min, max) pro generování pozic planet
-        velocity_range: dvojice čísel (min, max) pro generování rychlostí planet
-        mass_range: dvojice čísel (min, max) pro generování hmotností planet
+        number_of_bodies (int): kolik planet chci vygenerovat
+        position_range (Tuple[float, float]): dvojice čísel (min, max) pro generování pozic planet
+        velocity_range (Tuple[float, float]): dvojice čísel (min, max) pro generování rychlostí planet
+        mass_range (Tuple[float, float])): dvojice čísel (min, max) pro generování hmotností planet
 
     Returns:
-        names: seznam názvů těles
-        positions: numpy pole s pozicemi planet
-        velocities: numpy pole s rychlostmi planet
-        masses: numpy pole s hmotnostmi planet
+        names (List[str]): seznam názvů těles
+        positions (Tuple[float, float]): numpy pole s pozicemi planet
+        velocities (Tuple[float, float]): numpy pole s rychlostmi planet
+        masses (Tuple[float, float]): numpy pole s hmotnostmi planet
 
     """
     # generování názvů těles podle pořadí
-    names = [f"Planetka {i + 1}" for i in range(number_of_bodies)]
+    names: List[str] = [f"Planetka {i + 1}" for i in range(number_of_bodies)]
 
     # generování náhodných pozic planet
-    positions = np.random.uniform(low = position_range[0], high = position_range[1], size = (number_of_bodies, 2))
-    
+    positions: np.ndarray = np.random.uniform(
+        low=position_range[0], high=position_range[1], size=(number_of_bodies, 2))
+
     # generování náhodných rychlostí planet
-    velocities = np.random.uniform(low = velocity_range[0], high = velocity_range[1], size = (number_of_bodies, 2))
-    
+    velocities: np.ndarray = np.random.uniform(
+        low=velocity_range[0], high=velocity_range[1], size=(number_of_bodies, 2))
+
     # generování náhodných hmotností planet
-    masses = np.random.uniform(low = mass_range[0], high = mass_range[1], size = (number_of_bodies, 1))
-    
+    masses: np.ndarray = np.random.uniform(
+        low=mass_range[0], high=mass_range[1], size=(number_of_bodies, 1))
+
     return names, positions, velocities, masses
