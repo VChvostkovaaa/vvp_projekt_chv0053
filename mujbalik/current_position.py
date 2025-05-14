9424f4727915f99ae180a71a918b703633fa1096
VChvostkovaaa <chv0053@vsb.cz>
2025-05-14 10:54:06 +0200

    Update current_position.py

--- mujbalik/current_position.py
+++ mujbalik/current_position.py
@@ -1,29 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def current_positions(csv_path):
+
+def current_positions(csv_path: str) -> None:
     """
     Tahle funkce vzeme aktuální pozice planet z .csv souboru a vykreslí je do grafu.
 
     Args:
-        csv_path: cesta k .csv souboru s daty planet
+        csv_path (str): cesta k .csv souboru s daty planet
     """
 
     # načteme data z .csv souboru do tamulda DataFrame
-    df = pd.read_csv(csv_path)
+    df: pd.DataFrame = pd.read_csv(csv_path)
 
     # udělame si okno pro graf a nastavime velikost
-    plt.figure(figsize = (8, 8))
-    plt.title(f"Aktuální polohy planet", fontsize = 15)
+    plt.figure(figsize=(8, 8))
+    plt.title(f"Aktuální polohy planet", fontsize=15)
 
     for index, row in df.iterrows():    # iterrows: pandas -> umožnujemi procházet řádek po řádku
         # z každého řadku si vytáhneme potřebné informace
-        x = row['position_x']
-        y = row['position_y']
-        name = row['name']
+        x: float = row['position_x']
+        y: float = row['position_y']
+        name: int = row['name']
 
-        plt.scatter(x, y, label = name, s = 50)     # planeta -> jako tečka, label -> jméno planety, s -> velikost tečky
-        plt.text(x, y, name, fontsize = 9, ha = 'right', va = 'bottom')     # text -> jméno planety, fontsize -> velikost písma, ha/va -> zarovnání textu
+        # planeta -> jako tečka, label -> jméno planety, s -> velikost tečky
+        plt.scatter(x, y, label=name, s=50)
+        # text -> jméno planety, fontsize -> velikost písma, ha/va -> zarovnání textu
+        plt.text(x, y, name, fontsize=9, ha='right', va='bottom')
 
     # vypíšeme si popisky os a mřížku
     plt.xlabel("x position")
