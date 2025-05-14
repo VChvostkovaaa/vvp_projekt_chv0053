41d3ea5140760230a198c1e5330bc582fc66476d
VChvostkovaaa <chv0053@vsb.cz>
2025-05-14 11:00:26 +0200

    Update experiments.py

--- mujbalik/experiments.py
+++ mujbalik/experiments.py
@@ -1,23 +1,26 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from mujbalik.simulation import run_simulation
 
-def run_timestep_experiment(csv_path = 'data/planets.csv', n_steps = 365):
+
+def run_timestep_experiment(csv_path: str = 'data/planets.csv',
+                            n_steps: int = 365) -> None:
     """
     Spustí simulaci pro různé časové kroky a pro každé vykreslí výsledné dráhy planet.
 
     Args:
-        csv_path: cesta k CSV souboru s planetami
-        n_steps: počet kroků simulace
+        csv_path (str): cesta k CSV souboru s planetami
+        n_steps (int): počet kroků simulace
     """
     # načtení dat ze .csv souboru
-    df = pd.read_csv(csv_path)
-    positions_init = df[['position_x', 'position_y']].to_numpy()
-    velocities_init = df[['velocity_x', 'velocity_y']].to_numpy()
-    masses = df['mass'].to_numpy()
+    df: pd.DataFrame = pd.read_csv(csv_path)
+    positions_init: np.ndarray = df[['position_x', 'position_y']].to_numpy()
+    velocities_init: np.ndarray = df[['velocity_x', 'velocity_y']].to_numpy()
+    masses: np.ndarray = df['mass'].to_numpy()
 
     # různé hodnoty dt
-    dt_values = {
+    dt_values: dict[str, int] = {
         '1 hour': 60 * 60,
         '1 day': 60 * 60 * 24,
         '1 week': 60 * 60 * 24 * 7,
@@ -27,16 +30,17 @@ def run_timestep_experiment(csv_path = 'data/planets.csv', n_steps = 365):
     for label, dt in dt_values.items():
 
         # zkopíruj počáteční podmínky pro každou simulaci
-        positions = positions_init.copy()
-        velocities = velocities_init.copy()
+        positions: np.ndarray = positions_init.copy()
+        velocities: np.ndarray = velocities_init.copy()
 
-        history = run_simulation(positions, velocities, masses, dt, n_steps)
+        history: np.ndarray = run_simulation(
+            positions, velocities, masses, dt, n_steps)
 
         # vykresli výsledky
         plt.figure(figsize=(6, 6))
         for i in range(len(history)):
-            plt.plot(history[i][:, 0], history[i][:, 1], label=df['name'].iloc[i])
-
+            plt.plot(history[i][:, 0], history[i]
+                     [:, 1], label=df['name'].iloc[i])
 
         plt.title(f'Dráhy planet s dt = {label}')
         plt.xlabel('x position')
